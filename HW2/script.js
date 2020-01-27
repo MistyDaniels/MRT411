87d368eb75cc0bf2a6a2712d207cc22d66b3cecc
MistyDaniels <31575831+MistyDaniels@users.noreply.github.com>
2020-01-26 20:03:21 -0700

    Update script.js

--- HW2/script.js
+++ HW2/script.js
@@ -1,4 +1,4 @@
 
-// First part of JS Homework week 2
+// JS Homework week 2
 
 console.log("Favorite websites: 1. Tumblr.com, 2. Findtheinvisiblecow.com, youtube.com. Favorite Games: 1. Halo 2. Fallout 3. Minecraft. Favorite Artists: 1. Heather Snell 2.  Wendy Red Star 3. Rebecca Belmore.");
