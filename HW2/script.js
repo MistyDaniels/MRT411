bee5335748cbe271d7e365be6d8dc181bf1390c4
MistyDaniels <31575831+MistyDaniels@users.noreply.github.com>
2020-01-26 19:51:27 -0700

    Update script.js
    removed duplicate console.logs
    consolidated into single log.

--- HW2/script.js
+++ HW2/script.js
@@ -1,21 +1,4 @@
 
 // First part of JS Homework week 2
 
-console.log("Favorite websites:");
-console.log("1. Tumblr.com");
-console.log("2. findtheinvisiblecow.com");
-console.log("3. youtube.com");
-
-// Second part of JS Homework week 2
-
-console.log("Favorite games:");
-console.log("1. Entire Halo series");
-console.log("2. Fallout: New Vegas");
-console.log("3. Minecraft");
-
-// Third part of JS Homework week 2
-
-console.log("Favorite artists:");
-console.log("1. Heather Snell");
-console.log("2. Wendy Red Star");
-console.log("3. Rebecca Belmore")
+console.log("Favorite websites: 1. Tumblr.com, 2. Findtheinvisiblecow.com, youtube.com. Favorite Games: 1. Halo 2. Fallout 3. Minecraft. Favorite Artists: 1. Heather Snell 2.  Wendy Red Star 3. Rebecca Belmore.");
