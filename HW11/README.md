07fca205585cae2f5644cb095fed1e452c10561b
MistyDaniels <31575831+MistyDaniels@users.noreply.github.com>
2020-04-07 00:54:01 -0600

    updated README and data

--- HW11/README.md
+++ HW11/README.md
@@ -5,3 +5,13 @@ that I'll be able to navigate easily. I'll slowly fill each of these files as I 
 I did like the method I used last week where I used a practice file/folder set
 before incorporating them into my actual HW folders that I'll upload to gitHub.
 I'll be implementing that again.
+
+I dont know what it is right now but my fingers are being all jumbly and dont want
+to hit the right keys. Maybe too much caffeine. Definitely going to have to go through
+as best as I can letter and check for syntax errors as I'm sure I'll have one or two.
+
+I'm a little curious to see what would happen if I placed the three squares IN each
+other. Would I be able to clip them into each other or would the program just not work at all?
+
+I'm having trouble making my squares appear, I'm not sure why at these point. Going to keep
+poking at my code. I know I probably forgot to put something super simple, but we'll see.
---
+++ HW11/scripts/canvas.js
@@ -0,0 +1,132 @@
+var canvas;
+var ctx;
+var x = 50;
+var y = 50;
+var square1, square2;
+var direction;
+var questions;
+var squareArray = [];
+var lives = 5;
+$(document).ready(function(){
+  setup();
+  $(this).keypress(funcion(event){
+      getKey(event);
+  });
+});
+
+
+function setup()
+{
+  canvas = document.getElementById("myCanvas");
+  ctx = canvas.getContext("2d");
+
+  square1 = new Square(100,100,50,50,"#0000FF");
+  square2 = new Square(400,400,100,100,"#00FF00");
+   $.getJSON("data/information.json", function(data) {
+    for(var i = 0; i < data.squares.length; i++)
+    {
+      squareArray.push(new Square(data.squares[i].x, data.squares[i].y, data.squares[i].h, datasquares[i].w, data.squares[i].color));
+    }
+  drawSquare();
+  });
+}
+
+function getKey(event)
+  {
+    var char = event.which || event.keyCode;
+    var actualLetter = String.fromCharCode(char);
+    if(actualLetter == "w")
+    {
+      moveUp();
+      direction = "up";
+    }
+    if(actualLetter == "s")
+    {
+      moveDown();
+      direction = "down";
+    }
+    if(actualLetter == "a")
+    {
+      moveLeft();
+      direction = "left";
+    }
+    if(actualLetter == "d")
+    moveRight();
+    direction = "right";
+  }
+
+  fuction moveUp()
+  {
+    square1.y-=10;
+  }
+  fuction moveDown()
+  {
+    square1.y+=10;
+  }
+  fuction moveright()
+  {
+    square1.x+1=10;
+  }
+  function moveLeft()
+  {
+    square1.x-=10;
+  }
+
+  var test = hasCollided(square1,square2);
+  var test2 = false;
+  for(var i = 0; i < squareArray.length; i++)
+  {
+    test2 = hasCollided(square1,squareArray[i]);
+    if(test2 == true)
+    {
+      break;
+    }
+  }
+
+  if(test || test2)
+  {
+    lives--;
+    if(direction == "left")
+    {
+      moveRight();
+    }
+    else if(drection == "right")
+    {
+      moveLeft();
+    }
+    else if(direction == "up")
+    {
+      moveDown();
+    }
+    else if(direction == "down")
+    {
+      moveUp();
+    }
+  }
+
+  function drawSquare()
+  {
+    ctx.clearRect(0,0,800,600);
+    ctx.fillStyle = square1.mainColor;
+    ctx.fillRect(square1.x, square1.y, square1.width, square1.height);
+    ctx.fillStyle = square2.mainColor;
+    ctx.fillRect(square2.x, sqaure2.y, square2.width, square2.height);
+    for(var i = 0; i < squareArray.length; i++)
+    {
+      ctx.fillStyle = squareArray[i].maincolor;
+      ctx.fillRect(squareArray[i].x, squareArray[i].y, squareArray[i].width, squareArray[i].height)
+    }
+  }
+
+  ctx.font = "50px Arial";
+  ctx.fillText("Lives: " + lives, 10, 50);
+
+//DONT FORGET TO FIX GETTERS
+function hasCollided(object1, Object2) {
+  return !(
+    ((object1.y + object.height) < (object2.y)) ||
+    (object1.y > (object2.y + object2.height)) ||
+    ((object1.x + object.width) < object2.x) ||
+    (object1.x > (object2.x + object2.width))
+  );
+}
