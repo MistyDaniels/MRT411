921e81798379b8a4151750ba6e2878a9746fea33
MistyDaniels <31575831+MistyDaniels@users.noreply.github.com>
2020-04-06 23:30:20 -0600

    Updating code from Practice files

---
+++ HW11/README.md
@@ -0,0 +1,7 @@
+Late start but using some of the code from last week and the homework review
+from this week has been helpful.
+I'm of course starting by compiling all the needed folders and files in an order
+that I'll be able to navigate easily. I'll slowly fill each of these files as I need.
+I did like the method I used last week where I used a practice file/folder set
+before incorporating them into my actual HW folders that I'll upload to gitHub.
+I'll be implementing that again.
---
+++ HW11/scripts/square.js
@@ -0,0 +1,41 @@
+class Square{
+
+    constructor(xCoord,yCoord,objectHeight,objectWidth, color)
+    {
+      this.xCoord = xCoord;
+      this.xCoord = yCoord;
+      this.objectHeight = objectHeight;
+      this.objectWidth = objectWidth;
+      this.color = color;
+
+    }
+
+    get x()
+    {
+      return this.xCoord;
+    }
+    set x(value)
+    {
+      this.xCoord = value;
+    }
+    get y()
+    {
+      return this.yCoord;
+    }
+    set y(value)
+    {
+      this.yCoord = value;
+    }
+    get height()
+    {
+      return this.objectHeight;
+    }
+    get width()
+    {
+      return this.objectWidth;
+    }
+    get maincolor()
+    {
+      return this.color;
+    }
+}
